dbecff9a531548d3e9e2b4bb508d837675d49435
Sky <92821517+skyscool@users.noreply.github.com>
2022-07-08 21:45:36 +0200

    Update tw.js

--- sk/tw.js
+++ sk/tw.js
@@ -2,9 +2,7 @@ tailwind.config = {
     theme: {
       extend: {
         colors: {
-          "primary-dark": "#151420",
-          "primary-card": "rgb(97,81,207)",
-          "priamry-card-blue": "#303FE1",
+          "primary-dark": "#000000",
           "primary-light": "#29142b",
           "download": "rgb(97,81,207)",
           "primary-blue-light": "rgb(97,81,207)",
