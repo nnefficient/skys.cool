15a62b1812fa7d4d5c4258d5f26161f89e5602d8
Sky <92821517+skyscool@users.noreply.github.com>
2022-07-08 20:47:56 +0200

    Add files via upload

---
+++ sk/tw.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          "primary-dark": "#151420",
+          "primary-card": "rgb(97,81,207)",
+          "priamry-card-blue": "#303FE1",
+          "primary-light": "#29142b",
+          "download": "rgb(97,81,207)",
+          "primary-blue-light": "rgb(97,81,207)",
+          "langbg": "rgb(117,82,219)",
+          "langbg2": "rgb(150,117,219)",
+        },
+      },
+    },
+  };
