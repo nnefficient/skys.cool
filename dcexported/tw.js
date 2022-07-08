e3b9afccf53a411a895e553c1e689cc80a6195bd
Sky <92821517+skyscool@users.noreply.github.com>
2022-07-08 21:38:12 +0200

    Add files via upload

---
+++ dcexported/tw.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          "primary-dark": "#151420",
+          "primary-card": "rgb(97,81,207)",
+          "priamry-card-blue": "#303FE1",
+          "primary-light": "#29142b",
+          "download": "rgb(97,81,207)",
+          "primary-blue-light": "rgb(97,81,207)",
+        },
+      },
+    },
+  };
